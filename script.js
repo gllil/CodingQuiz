d31de1df6d7ab8576d920dc621d95dc714c30228
Gary Llil <garyjllil@gmail.com>
2020-02-05 16:44:49 -0500

    added JS for question 1 and event listeners for clicks. added check radials.

--- script.js
+++ script.js
@@ -1,47 +1,97 @@
- $("document").ready(function(){
-    var timer;
+
+    $(document).ready(function(){
+    var interval;
+    var count = 90;
     // questions and answers array
     var questions = [
        {
-         ques1: "What does HTML stand for?",
+         ques: "What does HTML stand for?",
          ansA: "A. Hyper Trainer Marking Language",
          ansB: "B. Hyper Text Marketing Language",
          ansC: "C. Hyper Text Markup Language",
-         ansD: "D. Hyper Text Markup Leveler"
+         ansD: "D. Hyper Text Markup Leveler",
+         correctAns: "C. Hyper Text Markup Language"
          // ansC: "C. Hyper Text Markup Language"
        },
        {
-         ques2: "<h1>Text</h1> is the correct way of making a header in HTML.",
+         ques: "<h1>Text</h1> is the correct way of making a header in HTML.",
          ansA: "A. True",
          ansB: "B. False",
+         correctAns: "A. True"
          // ansA: "A. True"
        },
        {
-         ques3: "Which of the following is the correct way of making a string in Java?",
+         ques: "Which of the following is the correct way of making a string in Java?",
          ansA: "A. String 'Text';",
          ansB: "B. String text = 'text';",
          ansC: "C. String text = 'text'",
-         ansD: "D. String text = (text);"
+         ansD: "D. String text = (text);",
+         correctAns: "B. String text = 'text';"
          // ansB: "B. String text = 'text';"
        },
        {
-         ques4: "In Java, a method is a container that holds classes.",
+         ques: "In Java, a method is a container that holds classes.",
          ansA: "A. True",
-         ansB: "B. False"
+         ansB: "B. False",
+         correctAns: "B. False"
          // ansB: "B. False"
        },
        {
-         ques5: "Is this the correct way to make an object in Java? Class class = new Class();",
+         ques: "Is this the correct way to make an object in Java? Class class = new Class();",
          ansA: "A. True",
-         ansB: "B. False"
+         ansB: "B. False",
+         correctAns: "A. True"
          // ansA: "A. True"
        }
 
-    ];
+    ]; console.log(questions)
+    $(".ansSelections").css("display", "none");
+    $("#buttonInfo").on("click", function(e){
+      e.preventDefault();
+      for(var i = 0; i < questions.length; i++){
+        $(".timeLeft").html("<h3>Seconds Left</h3>")
+        $("#buttonInfo").html("Submit");
+        $(".titleCard").html("<h1>Question 1</h1>");
+        $(".ansSelections").css("display", "block");
+        var question1 = questions[0];
+        $(".question").html(question1.ques);
+        $("#a").html(question1.ansA);
+        $("#b").html(question1.ansB);
+        $("#c").html(question1.ansC);
+        $("#d").html(question1.ansD);
 
-    $("#buttonInfo").on("click", function(){
-        $(".buttonSection").css("display", "none")
-    })
 
 
- })
+          $("#buttonInfo").on("click", function(e){
+            e.preventDefault();          
+          $("#radios").prop("checked", true);
+          if(question1.ansC === question1.correctAns){
+            alert("Correct");
+            localStorage.setItem("correct");
+          }else{
+            alert("Wrong");
+            localStorage.setItem("wrong");
+          };
+         
+        });
+      
+
+
+      };
+      timerStart();
+    });
+
+    function timerStart(){
+
+      interval = setInterval(function(){
+        if(count - 1 !== -1){
+          count--;
+          $(".timer").html(count);
+        } else{
+          clearInterval(interval);
+        };
+      }, 1000);
+    };
+
+
+  });
